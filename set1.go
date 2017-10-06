a0428d034163f833405b506824961e84e378d90b
Noah Lackstein <noah@lackstein.com>
2017-10-05 22:37:32 -0400

    Challenge 3

--- set1.go
+++ set1.go
@@ -3,7 +3,10 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"unicode/utf8"
 )
+
+type Corpus map[rune]float64
 
 func hexToBase64(hexString string) (string, error) {
 	res, err := hex.DecodeString(hexString)
@@ -24,4 +27,53 @@ func xor(a, b []byte) []byte {
 	}
 
 	return res
+}
+
+func buildCorpus(text string) Corpus {
+	totalChars := utf8.RuneCountInString(text)
+	corpus := make(Corpus, totalChars)
+
+	for _, char := range text {
+		corpus[char]++
+	}
+
+	for i := range corpus {
+		corpus[i] = corpus[i] / float64(totalChars)
+	}
+
+	return corpus
+}
+
+func scoreTextWithCorpus(text string, corpus Corpus) float64 {
+  var score float64
+  for _, char := range text {
+    score += corpus[char]
+  }
+
+  return score / float64(utf8.RuneCountInString(text))
+}
+
+func singleXOR(in []byte, key byte) []byte {
+  res := make([]byte, len(in))
+
+  for i := range in {
+    res[i] = in[i] ^ key
+  }
+
+  return res
+}
+
+func findSingleXORKey(in []byte, corpus Corpus) (res []byte, key byte, score float64) {
+  for char := range corpus {
+    out := singleXOR(in, byte(char))
+    s := scoreTextWithCorpus(string(out), corpus)
+
+    if s > score {
+      res = out
+      score = s
+      key = byte(char)
+    }
+  }
+
+  return
 }
--- set1_test.go
+++ set1_test.go
@@ -3,6 +3,7 @@ package cryptopals
 import (
 	"bytes"
 	"encoding/hex"
+	"io/ioutil"
 	"testing"
 )
 
@@ -13,6 +14,17 @@ func decodeHex(t *testing.T, s string) []byte {
 	}
 	return res
 }
+
+func corpusFromFile(fileName string) Corpus {
+	text, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	return buildCorpus(string(text))
+}
+
+var corpus = corpusFromFile("_testdata/sherlock.txt")
 
 func TestChallenge1(t *testing.T) {
 	res, err := hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
@@ -29,4 +41,9 @@ func TestChallenge2(t *testing.T) {
 	if !bytes.Equal(res, decodeHex(t, "746865206b696420646f6e277420706c6179")) {
 		t.Errorf("Wrong output: %x", res)
 	}
+}
+
+func TestChallenge3(t *testing.T) {
+	res, _, _ := findSingleXORKey(decodeHex(t, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"), corpus)
+	t.Logf("%s", res)
 }
